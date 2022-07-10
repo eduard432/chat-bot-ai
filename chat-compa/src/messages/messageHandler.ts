c4f3f8acf3396213efd462f54174caecd042c4a9
eduardo48 <juantokx@gmail.com>
2022-07-09 23:20:11 -0500

    Comentando el código

--- chat-compa/src/messages/messageHandler.ts
+++ chat-compa/src/messages/messageHandler.ts
@@ -13,7 +13,10 @@ const { err, grn, blu, log } = new Logger()
 
 const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 	try {
+		// Get the pgone number and msg
 		const phoneFrom = message.from
+		let msg: string = message.body
+
 		// const valid = await validatePhone(phoneFrom)
 
 		// // if (!valid) {
@@ -23,11 +26,12 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 		// // 	)
 		// // }
 
-		let msg: string = message.body
-
+		// The message hasMedia if true, download and analyze
 		if (message.hasMedia) {
 			msg = ((await mediaHandler(message)) || '') + ' ' + message.body
 		}
+
+		// Search for prev conversations
 
 		const document = await Conversation.findOne(
 			{ phone: phoneFrom },
@@ -39,6 +43,7 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 			}
 		)
 
+		// newMessage object from message
 		const newMessage = {
 			text: msg,
 			fromServer: false,
@@ -46,14 +51,18 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 
 		let history = ''
 
+		// If document exits
 		if (document) {
+			// Search from message limit, if true, send error
 			if (document?.messages.length >= 100)
 				return client.sendMessage(
 					phoneFrom,
 					errorMessageHandler['message-limit']
 				)
+			// Save prev messages on history
 			history = parseMessages(document?.messages)
 		} else {
+			// if doesnt exits, create
 			const newConversation = new Conversation({
 				phone: phoneFrom,
 			})
@@ -61,6 +70,7 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 			await newConversation.save()
 		}
 
+		// Find for conversation and update the stack
 		await Conversation.findOneAndUpdate(
 			{ phone: phoneFrom },
 			{
@@ -73,29 +83,39 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 			}
 		)
 
+		// Wait for random time, wait for more messages before response
 		await wait(random(2500, 4500))
 
+		// Get whatsapp chat, for the sendStateTyping()
 		const chat = await message.getChat()
 		chat.sendStateTyping()
 
+		// Update document, remove stack
 		const documentUpdates = await Conversation.findOneAndUpdate(
 			{ phone: phoneFrom },
 			{ lastMessages: [] }
 		)
 
+		// If stack is equal to 0 do nothing
 		if (documentUpdates?.lastMessages.length === 0) {
 			return
 		}
 
+		// Combine prevMessages with the stack
 		history += parseMessages(documentUpdates?.lastMessages)
 
+		// Send OpenAi for a AI Response:
 		let compl = await friendCompletition(history + 'Amigo:')
 
+		// Trim the response for spaces
 		compl = compl.trim()
 
+		// Simulate the waiting time of writing a message
 		await waitForTypeMessage(compl)
+
 		client.sendMessage(phoneFrom, compl)
 
+		// Push the message to the DB history of messages
 		await Conversation.findOneAndUpdate(
 			{ phone: phoneFrom },
 			{
@@ -106,6 +126,7 @@ const messageHandler = async (message: WAWebJS.Message, client: Client) => {
 		blu(history)
 		grn('Amigo: ' + compl)
 
+		// If AI Response, push the response to the DB history of messages
 		if (compl) {
 			await Conversation.findOneAndUpdate(
 				{ phone: phoneFrom },
